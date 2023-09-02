c45131bf19a429a2449b993deca2592a45f04215
Daniel Longacre <danieljlongacre@gmail.com>
2023-09-02 11:47:35 -0400

    Wrote starter shape classes and input prompt for inquirer

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter name of your logo (3 initials): ',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Enter the color of the text: ',
+            name: 'textcolor'
+        },
+        {
+            type: 'choice',
+            message: 'What shape do you want your logo: ',
+            choices: ['Triangle', 'Circle', 'Square'],
+            name: 'shape'
+        },
+        {
+            type: 'input',
+            message: 'Enter the color of the shape: ',
+            name: 'shapecolor'
+        },
+    ]) 
